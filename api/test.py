f59d7bfd729400bfd19c464d0452014c9e683be6
monishostwal <monishostwal@gmail.com>
2021-06-16 15:43:21 +0530

    Test Case file updated

--- api/test.py
+++ api/test.py
@@ -1,17 +1,25 @@
 import mlApi
 import unittest
 import requests
+import app
 
 class TestHelloWorld(unittest.TestCase):
+	def setUp(self):
+		self.app = app.app.test_client()
+		self.app.testing = True
 
-    def test_greeting_message(self):
+	def test_greeting_message(self):
 
-        headers = {'Content-Type': 'application/json'}
-
-        self.assertEqual(
-            requests.post('http://127.0.0.1:5000/flowers/predict',json= {
-    "feature":[1,2,3,4]}).json(),"virginica")
+	
+		self.assertEqual(
+		    requests.post('http://127.0.0.1:5000/flowers/predict',json= {
+		"feature":[1,2,3,4]}).json(),"virginica")
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+  
