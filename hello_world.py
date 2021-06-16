0d5a1d1d17a117a835610bc3f891e298cee3d3cc
Monish Ostwal <monishostwal@Monishs-MacBook-Pro.local>
2021-06-16 19:22:06 +0530

    Added training of model code also

--- hello_world.py
+++ hello_world.py
@@ -1,27 +1,27 @@
-from flask import Blueprint, request, jsonify,Flask
+from flask import Blueprint, request, jsonify, Flask
 import pickle
 import numpy
-
+import train_model
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 labels = {
-    0: "versicolor",
-    1: "setosa",
+    0: "setosa",
+    1: "versicolor",
     2: "virginica"
 }
 
+
 @app.route('/flowers/predict')
 def check():
+    model = train_model.train_model_flower()
     data = request.get_json()
     print(data)
-    data=numpy.array(data['feature'])
-    data=data.reshape(1,-1)
+    data = numpy.array(data['feature'])
+    data = data.reshape(1, -1)
     print(data)
     predictions = model.predict(data)
     print(predictions)
     return jsonify(labels[predictions[0]])
-
 
 
 if __name__ == '__main__':
---
+++ train_model.py
@@ -0,0 +1,21 @@
+import pickle
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+
+def train_model_flower():
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+    labels = {
+        0: "setosa",
+        1: "versicolor",
+        2: "virginica"
+    }
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.5)
+    from sklearn import tree
+    classifier = tree.DecisionTreeClassifier()
+    classifier.fit(x_train, y_train)
+    pickle.dump(classifier, open('model.pkl', 'wb'))
+    model = pickle.load(open('model.pkl', 'rb'))
+    return model
