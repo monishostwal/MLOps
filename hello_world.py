2b44b3a3c027b49563737b87884d9b245c56d6a3
monishostwal <monishostwal@gmail.com>
2021-06-16 17:35:21 +0530

    added my own code

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Blueprint, request, jsonify,Flask
+import pickle
+import numpy
+
 
 app = Flask(__name__)
-
+model = pickle.load(open('model.pkl', 'rb'))
+labels = {
+    0: "versicolor",
+    1: "setosa",
+    2: "virginica"
+}
 def generate_html(message):
     version_number = '0001'
     html = """
@@ -25,6 +33,18 @@ def greet():
 def hello_world():
     html = generate_html(greet())
     return html
+    
+@app.route('/flowers/predict')
+def check():
+    data = request.get_json()
+    print(data)
+    data=numpy.array(data['feature'])
+    data=data.reshape(1,-1)
+    print(data)
+    predictions = model.predict(data)
+    print(predictions)
+    return jsonify(labels[predictions[0]])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
