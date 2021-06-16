260ee30c795dd7523ba8bc60cf2ac2e2d60e2fd3
monishostwal <monishostwal@gmail.com>
2021-06-16 16:08:43 +0530

    Changes in test

---
+++ mlApi.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+import pickle
+import numpy
+
+mlApi = Blueprint('mlApi', __name__)
+model = pickle.load(open('model.pkl', 'rb'))
+labels = {
+    0: "versicolor",
+    1: "setosa",
+    2: "virginica"
+}
+
+
+@mlApi.route("/predict",methods=['POST'])
+def predict():
+    data = request.get_json()
+    print(data)
+    data=numpy.array(data['feature'])
+    data=data.reshape(1,-1)
+    print(data)
+    predictions = model.predict(data)
+    print(predictions)
+    return jsonify(labels[predictions[0]])
