fee0301025ba4f466879de19f60a62efe0175fcd
monishostwal <monishostwal@gmail.com>
2021-06-16 17:44:06 +0530

    added own test case

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,6 +1,6 @@
 import hello_world
 import unittest
-
+import requests
 class TestHelloWorld(unittest.TestCase):
 
     def setUp(self):
@@ -14,6 +14,11 @@ class TestHelloWorld(unittest.TestCase):
     def test_greeting_message(self):
         greeting = 'Welcome to CI/CD'
         self.assertEqual(hello_world.greet(), greeting)
+
+    def test_model_predictions(self):
+        self.assertEqual(
+ requests.get('http://127.0.0.1:5000/flowers/predict',json= {
+"feature":[1,2,3,4]}).json(),"virginica")
 
 if __name__ == '__main__':
     unittest.main()
