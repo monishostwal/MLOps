a5cf416db6503be03d365a2ca5b4c9daf8cc1754
monishostwal <monishostwal@gmail.com>
2021-06-16 19:28:51 +0530

    changed file names and structure of code

--- test_mlApi.py
+++ test_mlApi.py
@@ -1,18 +1,20 @@
-import hello_world
+import mlApi
 import unittest
 import requests
+
+
 class TestHelloWorld(unittest.TestCase):
 
     def setUp(self):
-        self.app = hello_world.app.test_client()
+        self.app = mlApi.app.test_client()
         self.app.testing = True
 
     def test_status_code(self):
-        response = self.app.get('/flowers/predict',json= {
-"feature":[1,2,3,4]})
+        response = self.app.get('/flowers/predict', json={
+            "feature": [1, 2, 3, 4]})
 
         self.assertEqual(response.json, 'virginica')
-    
+
 
 if __name__ == '__main__':
     unittest.main()
